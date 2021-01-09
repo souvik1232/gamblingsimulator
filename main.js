4ce1e18bae3bf2cbcd05a4c2aca17cb4b28c85cf
souvik1232 <souvik199831@gmail.com>
2021-01-09 10:46:37 +0530

    Added:Logic to ask user to simulate again

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const readline = require('readline-sync')
 const gamblng = require('./utility')
-console.log("Welcome to Gambling Simulator");
-gamblng.gamble();
+let reply;
+do {
+    console.log("Welcome to Gambling Simulator");
+    gamblng.gamble();
+    reply = readline.question("Do you want to try again? ");
+} while (reply == 'Y');
+
  
