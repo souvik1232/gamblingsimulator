85cbf28862dd10bdd6f1fc5909780aaf6a40f912
souvik1232 <souvik199831@gmail.com>
2021-01-09 09:50:08 +0530

    Added:Logic to find total win or loss

--- utility.js
+++ utility.js
@@ -3,17 +3,34 @@ const BET = 1;
 
 class Gambling {
       gamble = () =>{
+           let day = 0;
            let stake = STAKE;
            let bet = BET;
-           let luck = Math.floor(Math.random() * 2);
-           if (luck == 1) {
+           let totalWin=0;
+           let totalLoss=0;
+           do {
+                day++;
+                let luck = Math.floor(Math.random() * 2);
+               if (luck == 1) {
                 console.log("Win");
                 stake = stake+bet;
-           } else if (luck == 0) {
+                totalWin = totalWin+(stake/2);
+                stake = stake+(stake/2);
+                //console.log("Win->",day,"day","->",stake);
+               } else if (luck == 0) {
                 console.log("Loss");
                 stake=stake-bet;
+                totalLoss = totalLoss+(stake/2);
                 stake = stake/2;
-          }
+                if (stake<=0) {
+                     break;
+                }
+                //console.log("Loss->",day,"day","->",stake);
+               }
+           } while (day<20);
+           console.log("Total win ->",totalWin,"$");
+           console.log("Total loss ->",totalLoss,"$")
+           
       }
 }
 module.exports = new Gambling();
