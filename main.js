98081f80170c10d8ce294316bc53f2b9836e669b
souvik1232 <souvik199831@gmail.com>
2021-01-09 09:13:10 +0530

    Added:Function to simulate win or loss of the bet

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const readline = require('readline-sync')
+const gamblng = require('./utility')
 console.log("Welcome to Gambling Simulator");
+gamblng.gamble();
  
--- utility.js
+++ utility.js
@@ -2,5 +2,13 @@ const STAKE = 100;
 const BET = 1;
 
 class Gambling {
-      
+      gamble = () =>{
+          let bet = Math.floor(Math.random() * 2);
+          if (bet == 1) {
+               console.log("Win");
+          } else if (bet == 0) {
+               console.log("Loss");
+          }
+      }
 }
+module.exports = new Gambling();
